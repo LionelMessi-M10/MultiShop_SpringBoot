8565297e892bb06245c0af7a57816d5581bc6bf8
LionelMessi-M10 <luonghuuvuong1962@gmail.com>
2025-06-17 18:04:49 +0700

    Update

--- src/main/java/com/multishop/entity/Shipping.java
+++ src/main/java/com/multishop/entity/Shipping.java
@@ -1,6 +1,11 @@
 package com.multishop.entity;
 
+import com.multishop.enums.ShippingMethod;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -15,9 +20,16 @@ public class Shipping extends Base {
     @OneToOne
     @JoinColumn(name = "order_id")
     private Order order;
+    
+    private String address; // Dia chi giao hang
+    private String city; // thanh pho
+    private String state; // bang hoac khu vuc
+    private String postalCode; // ma buu dien
+    private String country; // quoc gia
+    @Column(name = "shipping_method")
+	@Enumerated(EnumType.STRING)
+    private Enum<ShippingMethod> shippingMethods; // phuong thuc van chuyen
 
-    private String shipCode;
     private String provider;
     private Double fee;
-    private String statusShipping;
 }
---
+++ src/main/java/com/multishop/enums/ShippingMethod.java
@@ -0,0 +1,5 @@
+package com.multishop.enums;
+
+public enum ShippingMethod {
+
+}
