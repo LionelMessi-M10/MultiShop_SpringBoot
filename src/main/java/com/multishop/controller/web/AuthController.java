a2d399ba4fee9a1afd61743d2a437a26b73d894b
LionelMessi-M10 <luonghuuvuong1962@gmail.com>
2025-09-15 14:43:43 +0700

    Update api

--- src/main/java/com/multishop/controller/web/AuthController.java
+++ src/main/java/com/multishop/controller/web/AuthController.java
@@ -16,7 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.multishop.entity.Role;
 import com.multishop.entity.User;
 import com.multishop.enums.ERole;
-import com.multishop.model.request.RegistrationRequest;
+import com.multishop.model.request.AuthenticationRequest;
+import com.multishop.model.request.UserRequest;
 import com.multishop.model.response.AuthenticationResponse;
 import com.multishop.repository.RoleRepository;
 import com.multishop.repository.UserRepository;
@@ -38,34 +39,34 @@ public class AuthController {
     private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody RegistrationRequest authenticationRequest) {
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) {
         try {
             authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(authenticationRequest.getEmal(), authenticationRequest.getPassword())
+                new UsernamePasswordAuthenticationToken(authenticationRequest.getEmail(), authenticationRequest.getPassword())
             );
         } catch (BadCredentialsException e) {
             return ResponseEntity.badRequest().body("Incorrect username or password");
         }
-        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getEmal());
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getEmail());
         final String jwt = jwtUtil.generateToken(userDetails);
         return ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody RegistrationRequest registrationRequest) {
+    public ResponseEntity<?> registerUser(@RequestBody UserRequest userRequest) {
         // 1. Kiểm tra username đã tồn tại chưa
-        if (userRepository.findByUsername(registrationRequest.getEmal()).isPresent()) {
+        if (userRepository.findByUsername(userRequest.getEmail()).isPresent()) {
             return ResponseEntity.badRequest().body("Username is already taken!");
         }
 
         // 2. Tạo một đối tượng User mới
         User user = new User();
-        user.setEmail(registrationRequest.getEmal());
-        user.setPassword(passwordEncoder.encode(registrationRequest.getPassword()));
+        user.setEmail(userRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(userRequest.getPassword()));
 
         // 3. Gán vai trò (Role) mặc định
         // Tìm vai trò 'SELLER' hoặc 'BUYER' trong database
-        Role sellerRole = roleRepository.findByCode(ERole.valueOf(registrationRequest.getRoleName())).orElseThrow(() -> new RuntimeException("Role not found!"));
+        Role sellerRole = roleRepository.findByCode(ERole.valueOf(userRequest.getRoleCode())).orElseThrow(() -> new RuntimeException("Role not found!"));
         
         user.setRoles(Collections.singleton(sellerRole));
 
