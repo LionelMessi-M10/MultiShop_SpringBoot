8039b3029598297cf1d9a1fcb0640f478a0fc3f5
LionelMessi-M10 <luonghuuvuong1962@gmail.com>
2025-09-26 16:19:16 +0700

    Update controller

--- src/main/java/com/multishop/controller/shop/CategoryController.java
+++ src/main/java/com/multishop/controller/shop/CategoryController.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +38,9 @@ public class CategoryController {
 	    );
 	}
 
-	
+	@PostMapping("/create")
+	public ResponseEntity<ApiResponse> updateCategory() {
+		return null;
+	}
 	
 }
